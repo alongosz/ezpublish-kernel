3a67d819fedac7fdbdc329d211e5336bc12dc124
Andrew Longosz <andrzej.longosz@ez.no>
2016-09-01 14:51:33 +0200

    EZP-26098: Implemented \eZ\Publish\SPI\Search\Indexing\ContentIndexing on the Legacy Search Engine handler

--- eZ/Publish/Core/Search/Legacy/Content/Handler.php
+++ eZ/Publish/Core/Search/Legacy/Content/Handler.php
@@ -25,6 +25,7 @@ use eZ\Publish\Core\Base\Exceptions\NotFoundException;
 use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
 use eZ\Publish\SPI\Persistence\Content\Language\Handler as LanguageHandler;
 use eZ\Publish\Core\Search\Legacy\Content\Mapper\FullTextMapper;
+use eZ\Publish\SPI\Search\Indexing\ContentIndexing;
 
 /**
  * The Content Search handler retrieves sets of of Content objects, based on a
@@ -47,7 +48,7 @@ use eZ\Publish\Core\Search\Legacy\Content\Mapper\FullTextMapper;
  * content objects based on criteria, which could not be converted in to
  * database statements.
  */
-class Handler implements SearchHandlerInterface
+class Handler implements SearchHandlerInterface, ContentIndexing
 {
     /**
      * Content locator gateway.
@@ -392,11 +393,9 @@ class Handler implements SearchHandlerInterface
     }
 
     /**
-     * Commits the data to the index, making it available for search.
-     *
-     * @param bool $flush
+     * {@inheritdoc}
      */
-    public function commit($flush = false)
+    public function commit()
     {
         // Not needed with Legacy Storage/Search Engine
     }
